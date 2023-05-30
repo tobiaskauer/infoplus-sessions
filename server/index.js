3f33247d4519f35cedc0acfd7fd31b6a8420fa03
Tobias Kauer <tobias.kauer@fh-potsdam.de>
2023-05-30 12:28:53 +0200

    don't show sessions without category

--- server/index.js
+++ server/index.js
@@ -1,66 +1,61 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const mongoSanitize = require('express-mongo-sanitize')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const VisitorRoutes = require('./routes/visitor.route')
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-var morgan = require('morgan')
-require('dotenv').config();
-const ENVIRONMENT = process.env.ENVIRONMENT || 'PRODUCTION';
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const mongoSanitize = require("express-mongo-sanitize");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const VisitorRoutes = require("./routes/visitor.route");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+var morgan = require("morgan");
+require("dotenv").config();
+const ENVIRONMENT = process.env.ENVIRONMENT || "PRODUCTION";
 
-if(ENVIRONMENT == "PRODUCTION") {
-  app.use(morgan('common', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}))
+if (ENVIRONMENT == "PRODUCTION") {
+  app.use(
+    morgan("common", {
+      stream: fs.createWriteStream("./access.log", { flags: "a" }),
+    })
+  );
 } else {
-  app.use(morgan('dev',{}))
+  app.use(morgan("dev", {}));
 }
 
-  
+app.use(cors());
+app.use(bodyParser.json());
+app.use(mongoSanitize({ replaceWith: "_" }));
+app.use("/api/visitor", VisitorRoutes);
 
+const credentials = {};
 
+if (ENVIRONMENT == "PRODUCTION") {
+  const CREDDIR = process.env.CREDDIR;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(mongoSanitize({replaceWith: '_'}))
-app.use('/api/visitor', VisitorRoutes)
+  const privateKey = fs.readFileSync(CREDDIR + "privkey.pem", "utf8");
+  const certificate = fs.readFileSync(CREDDIR + "cert.pem", "utf8");
+  const ca = fs.readFileSync(CREDDIR + "chain.pem", "utf8");
 
-const credentials = {} 
-
-if(ENVIRONMENT == "PRODUCTION") {
-  const CREDDIR = process.env.CREDDIR
-  
-  const privateKey = fs.readFileSync(CREDDIR+'privkey.pem', 'utf8');
-  const certificate = fs.readFileSync(CREDDIR+'cert.pem', 'utf8');
-  const ca = fs.readFileSync(CREDDIR+'chain.pem', 'utf8');
-
-	credentials.key = privateKey,
-	credentials.cert = certificate,
-	credentials.ca = ca
+  (credentials.key = privateKey),
+    (credentials.cert = certificate),
+    (credentials.ca = ca);
 }
-
 
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-
-
-    })
-    .then(() => console.log('MongoDB database Connected...'))
-    .catch((err) => console.log(err))
-
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB database Connected..."))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 8443;
-if(credentials.cert) {
+if (credentials.cert) {
   var httpsServer = https.createServer(credentials, app);
   httpsServer.listen(PORT);
   console.log(`HTTPS is running on port ${PORT}.`);
 } else {
-    var httpServer = http.createServer(app);
-    httpServer.listen(PORT);
-    console.log(`HTTP is running on port ${PORT}.`);
+  var httpServer = http.createServer(app);
+  httpServer.listen(PORT);
+  console.log(`HTTP is running on port ${PORT}.`);
 }
-
--- server/routes/visitor.route.js
+++ server/routes/visitor.route.js
@@ -1,61 +1,61 @@
-const { Router } = require('express')
-const Visitor = require('../models/visitor.model')
-const router = Router()
-const releaseDate = '2023-04-13'
+const { Router } = require("express");
+const Visitor = require("../models/visitor.model");
+const router = Router();
+const releaseDate = "2023-04-13";
 
-router.get('/', async (req, res) => {
-    try {
-        const visitors = await Visitor.find({
-            "createdAt": {$gte: releaseDate},
-            "duration": { $gt: 0} 
-            //"category":{$neq: "practitioner"}
+router.get("/", async (req, res) => {
+  try {
+    const visitors = await Visitor.find(
+      {
+        createdAt: { $gte: releaseDate },
+        duration: { $gt: 0 },
+        category: { $ne: null },
+        //"category":{$neq: "practitioner"}
+      },
+      "-_id category duration"
+    );
+    if (!visitors) throw new Error("No Visitors found");
+    res.status(200).json(visitors);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-        },'-_id category duration')
-        if (!visitors) throw new Error('No Visitors found')
-        res.status(200).json(visitors)
-        
-    
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+router.post("/", async (req, res) => {
+  const newVisitor = new Visitor(req.body);
+  try {
+    const visitor = await newVisitor.save();
+    if (!visitor) throw new Error("Something went wrong saving the Visitor");
+    res.status(200).json(visitor);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.post('/', async (req, res) => {
-    const newVisitor = new Visitor(req.body)
-    try {
-        const visitor = await newVisitor.save()
-        if (!visitor) throw new Error('Something went wrong saving the Visitor')
-        res.status(200).json(visitor)
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: error.message })
-    }
-})
+router.post("/:id", async (req, res) => {
+  try {
+    const updatedVisitor = await Visitor.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedVisitor);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.post('/:id', async (req, res) => {
-    try {
-        const updatedVisitor = await Visitor.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {new: true}
-            )
-        res.status(200).json(updatedVisitor)
-    } catch (error) {
-        console.log(error) 
-        res.status(500).json({ message: error.message })
-    }
-})
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const removed = await Todo.findByIdAndDelete(id);
+    if (!removed) throw Error("Something went wrong ");
+    res.status(200).json(removed);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const removed = await Todo.findByIdAndDelete(id)
-        if (!removed) throw Error('Something went wrong ')
-        res.status(200).json(removed)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
-module.exports = router
+module.exports = router;
